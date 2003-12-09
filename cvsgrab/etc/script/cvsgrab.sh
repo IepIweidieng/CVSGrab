c3982a38e582b00e1dac456e324c92a78a768b25
Ludovic Claude <ludovicc@users.sourceforge.net>
2003-12-09 00:23:54 +0000

    last minute changes for release 2.0.1

--- cvsgrab/etc/script/cvsgrab.sh
+++ cvsgrab/etc/script/cvsgrab.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+#   Copyright (c) 2001-2002 The Apache Software Foundation.  All rights
+#   reserved.
 # OS specific support
 cygwin=false;
 darwin=false;
@@ -7,19 +9,61 @@ case "`uname`" in
   CYGWIN*) cygwin=true ;;
   Darwin*) darwin=true
            if [ -z "$JAVA_HOME" ] ; then
-             JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+             JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home   
            fi
            ;;
 esac
 
-# For Cygwin, ensure paths are in UNIX format
+if [ -z "$CVSGRAB_HOME" ] ; then
+  # try to find CVSGRAB
+  if [ -d /opt/cvsgrab ] ; then 
+    CVSGRAB_HOME=/opt/cvsgrab
+  fi
+
+  if [ -d "${HOME}/opt/cvsgrab" ] ; then 
+    CVSGRAB_HOME="${HOME}/opt/cvsgrab"
+  fi
+
+  ## resolve links - $0 may be a link to cvsgrab's home
+  PRG="$0"
+  progname=`basename "$0"`
+  saveddir=`pwd`
+
+  # need this for relative symlinks
+  cd `dirname "$PRG"`
+  
+  while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '.*/.*' > /dev/null; then
+	PRG="$link"
+    else
+	PRG=`dirname "$PRG"`"/$link"
+    fi
+  done
+  
+  CVSGRAB_HOME=`dirname "$PRG"`/..
+
+  cd "$saveddir"
+
+  # make it fully qualified
+  CVSGRAB_HOME=`cd "$CVSGRAB_HOME" && pwd`
+fi
+
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+
 if $cygwin ; then
-  [ -n "$ANT_HOME" ] &&
+  [ -n "$CVSGRAB_HOME" ] &&
+    CVSGRAB_HOME=`cygpath --unix "$CVSGRAB_HOME"`
+  [ -n "$JAVA_HOME" ] &&
     JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
   [ -n "$CLASSPATH" ] &&
     CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
 fi
     
+# set CVSGRAB_LIB location
+CVSGRAB_LIB="${CVSGRAB_HOME}/lib"
+
 if [ -z "$JAVACMD" ] ; then 
   if [ -n "$JAVA_HOME"  ] ; then
     if [ -x "$JAVA_HOME/jre/sh/java" ] ; then 
@@ -47,25 +91,26 @@ if [ -n "$CLASSPATH" ] ; then
 fi
 
 # add in the dependency .jar files in non-RPM mode (the default)
-DIRLIBS=./lib
-for i in "${DIRLIBS}"/*.jar
+for i in "${CVSGRAB_LIB}"/*.jar
 do
   # if the directory is empty, then it will return the input string
   # this is stupid, so case for it
-  if [ "$i" != "${DIRLIBS}/*.jar" ] ; then
+  if [ -f "$i" ] ; then
     if [ -z "$LOCALCLASSPATH" ] ; then
-      LOCALCLASSPATH=$i
-    else
+      LOCALCLASSPATH="$i"
+     else
       LOCALCLASSPATH="$i":"$LOCALCLASSPATH"
     fi
   fi
 done
 
-# For Cygwin, switch paths to Windows format
-if $Cygwin; then
-  JAVA_HOME=`cygpath --windows "$JAVA_HOME"`
+# For Cygwin, switch paths to Windows format before running java
+if $cygwin; then
+  CVSGRAB_HOME=`cygpath --path --windows "$CVSGRAB_HOME"`
+  JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
   CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
   LOCALCLASSPATH=`cygpath --path --windows "$LOCALCLASSPATH"`
+  CYGHOME=`cygpath --path --windows "$HOME"`
 fi
 
 LOG_ARGS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog -Dorg.apache.commons.logging.simplelog.showShortLogname=false"
